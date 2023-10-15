a6b982a7d9eb022b9c8ffc96aad17cbef0cc162c
devhoss <velasco2288@gmail.com>
2023-10-15 08:52:40 -0500

    hola Mundo

--- src/main/java/com/devhoss/app/serviciofraudes/ServicioFraudesApplication.java
+++ src/main/java/com/devhoss/app/serviciofraudes/ServicioFraudesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServicioFraudesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServicioFraudesApplication.class, args);
+	System.out.println("Hola Mundo Fraudes!!");
 	}
 
 }
