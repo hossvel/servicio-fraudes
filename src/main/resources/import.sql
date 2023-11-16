29241ed64c27617f125480c2f0c8a75adc13c2c4
devhoss <velasco2288@gmail.com>
2023-11-15 22:27:00 -0500

    configure postgres

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
-INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('101', '12', '10', '1001');
-INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('102', '14', '50', '1002');
-INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('103', '15', '30', '1003');
-INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('104', '16', '50', '1004');
-INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('105', '16', '80', '1005');
+INSERT INTO tbl_fraud_customer_check (id, tipo,valor, clienteid) VALUES ('101', '12', '10', '1001');
+--INSERT INTO tbl_fraud_customer_check (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+--INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('102', '14', '50', '1002');
+--INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('103', '15', '30', '1003');
+--INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('104', '16', '50', '1004');
+--INSERT INTO tbl_fraud_customer_check (`id`, `tipo`, `valor`, `clienteid`) VALUES ('105', '16', '80', '1005');
